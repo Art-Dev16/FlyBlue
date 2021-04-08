3a9ac27a8e579d192ce420aa42eb0c473dbe65ce
Art-Dev16 <78432088+Art-Dev16@users.noreply.github.com>
2021-04-08 14:28:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,70 @@
+
+
+function aff_rectres(){
+
+
+	if(document.getElementById("rectres").style.visibility == "hidden")
+	{
+
+		document.getElementById("rectres").style.visibility = "visible";
+	}
+
+	else
+	{
+
+		document.getElementById("rectres").style.visibility = "hidden";
+		document.getElementById("rectconf").style.visibility = "hidden";
+	}
+	return true;
+
+}
+
+var destination = "Aucune Destination";
+console.log(destination);
+
+function houra1(){
+	destination = "Fidji";
+	document.getElementById("rectconf").style.visibility = "visible";
+	document.getElementById("affdest").innerHTML = destination;
+	
+	
+}
+function houra2(){
+	destination = "Pologne";
+	document.getElementById("rectconf").style.visibility = "visible";
+	document.getElementById("affdest").innerHTML = destination;
+	
+	
+}
+function houra3(){
+	destination = "National Park Service, Etats-Unis";
+	document.getElementById("rectconf").style.visibility = "visible";
+	document.getElementById("affdest").innerHTML = destination;
+	
+	
+}
+function houra4(){
+	destination = "Groenland";
+	document.getElementById("rectconf").style.visibility = "visible";
+	document.getElementById("affdest").innerHTML = destination;
+
+	
+}
+
+
+function affprix(){
+	recup = document.getElementById("number").value;
+	montant =  recup*=250;
+	if (document.getElementById('ere').checked){
+		montant*=1.5;
+	}
+	document.getElementById("prix").innerHTML = montant;
+
+}
+	
+function prixdiv(){
+	montant /=1.5;
+	document.getElementById("prix").innerHTML = montant;
+
+
+}
